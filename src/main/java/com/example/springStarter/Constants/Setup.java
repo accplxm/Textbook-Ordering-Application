cf83bd435685165902149be37735f453359d3da0
srinivasnuvvula <snuvvula@murraystate.edu>
2017-11-03 18:29:45 -0500

    Change Redirect URL
    Redirect URL change

--- src/main/java/com/example/springStarter/Constants/Setup.java
+++ src/main/java/com/example/springStarter/Constants/Setup.java
@@ -3,6 +3,6 @@ package com.example.springStarter.Constants;
 public  final class Setup {
   public static final String CLIENT_ID = "1057193750796-g5f00lio8es15uulbfq4rs864a34nmsn.apps.googleusercontent.com";
   public static final String CLIENT_SECRET = "LQZYvwxVAR4psGsq77FURmng";
-  public static final String REDIRECT_URL = "http://localhost:8080/oauth2callback";
+  public static final String REDIRECT_URL = "https://textbook-ordering-application.herokuapp.com/oauth2callback";
   public static final String GOOGLE_AUTH_URL="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri="+REDIRECT_URL+"&response_type=code&client_id="+CLIENT_ID+"&approval_prompt=force";
 }
